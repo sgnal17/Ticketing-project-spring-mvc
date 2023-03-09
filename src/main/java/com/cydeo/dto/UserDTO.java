62c4fb24d5588bf745bf19706055a31785437dfd
sgnal17 <96966147+sgnal17@users.noreply.github.com>
2023-03-09 11:17:26 -0500

    DTO classes added

---
+++ src/main/java/com/cydeo/dto/UserDTO.java
@@ -0,0 +1,19 @@
+package com.cydeo.dto;
+
+import com.cydeo.enums.Gender;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDTO {
+    private String firstName;
+    private String lastName;
+    private String password;
+    private boolean enabled;
+    private String phone;
+    private RoleDTO role;
+    private Gender gender;
+}
---
+++ src/main/java/com/cydeo/entity/User.java
@@ -0,0 +1,37 @@
+package com.cydeo.entity;
+
+import com.cydeo.enums.Gender;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+
+@Data
+@NoArgsConstructor
+
+public class User extends BaseEntity {
+    private String firstName;
+    private String lastName;
+    private String username;
+    private String password;
+    private boolean enabled;
+    private String phone;
+    private Role role;
+    private Gender gender;
+
+    public User(Long id, LocalDateTime insertDateTime, Long insertUserId, LocalDateTime lastUpdateDateTime, Long lastUpdateUserId, String firstName, String lastName, String username, String password, boolean enabled, String phone, Role role, Gender gender) {
+        super(id, insertDateTime, insertUserId, lastUpdateDateTime, lastUpdateUserId);
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.username = username;
+        this.password = password;
+        this.enabled = enabled;
+        this.phone = phone;
+        this.role = role;
+        this.gender = gender;
+    }
+
+
+}
