aea8a2e90b304461f0076ceb3f0a88368f86e18c
sgnal17 <96966147+sgnal17@users.noreply.github.com>
2023-03-09 11:23:14 -0500

    Service interfaces

--- src/main/java/com/cydeo/service/CrudService.java
+++ src/main/java/com/cydeo/service/CrudService.java
@@ -5,11 +5,8 @@ import java.util.List;
 public interface CrudService<T,ID> {
 
     T save(T t);
-
     T findById(ID id);
-
     List<T> findAll();
-
     void deleteById(ID id);
 
 }
--- src/main/java/com/cydeo/service/RoleService.java
+++ src/main/java/com/cydeo/service/RoleService.java
@@ -3,7 +3,7 @@ package com.cydeo.service;
 import com.cydeo.dto.RoleDTO;
 
 
-import java.util.List;
+
 
 public interface RoleService extends CrudService<RoleDTO,Long> {
 
