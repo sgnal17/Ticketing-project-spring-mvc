435d23eb03c4699c934e941d0676997ec94a7259
sgnal17 <96966147+sgnal17@users.noreply.github.com>
2023-03-13 18:45:49 -0400

    ProjectService and impl added

---
+++ src/main/java/com/cydeo/service/impl/ProjectServiceImpl.java
@@ -0,0 +1,41 @@
+package com.cydeo.service.impl;
+
+import com.cydeo.dto.ProjectDTO;
+import com.cydeo.enums.Status;
+import com.cydeo.service.ProjectService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
+public class ProjectServiceImpl extends AbstractMapService<ProjectDTO,String> implements ProjectService {
+    @Override
+    public ProjectDTO save(ProjectDTO projectDTO) {
+
+        if(projectDTO.getStatus()==null)
+            projectDTO.setStatus(Status.OPEN);
+
+        return super.save(projectDTO.getProjectCode(), projectDTO);
+    }
+
+    @Override
+    public ProjectDTO findById(String projectCode) {
+        return super.findById(projectCode);
+    }
+
+    @Override
+    public List<ProjectDTO> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(String projectCode) {
+        super.deleteById(projectCode);
+    }
+
+    @Override
+    public void update(ProjectDTO object) {
+        super.update(object.getProjectCode(), object);
+    }
+
+
+}
