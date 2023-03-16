42c1b91d68368d31c61d3749fe7b1e0a39ee25c0
sgnal17 <96966147+sgnal17@users.noreply.github.com>
2023-03-16 12:29:11 -0400

    new service method added

--- src/main/java/com/cydeo/service/impl/ProjectServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/ProjectServiceImpl.java
@@ -1,13 +1,26 @@
 package com.cydeo.service.impl;
 
 import com.cydeo.dto.ProjectDTO;
+import com.cydeo.dto.TaskDTO;
+import com.cydeo.dto.UserDTO;
 import com.cydeo.enums.Status;
 import com.cydeo.service.ProjectService;
+import com.cydeo.service.TaskService;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 public class ProjectServiceImpl extends AbstractMapService<ProjectDTO,String> implements ProjectService {
+
+    private final TaskService taskService;
+
+    public ProjectServiceImpl(TaskService taskService) {
+        this.taskService = taskService;
+    }
+
+
     @Override
     public ProjectDTO save(ProjectDTO projectDTO) {
 
@@ -45,4 +58,27 @@ public class ProjectServiceImpl extends AbstractMapService<ProjectDTO,String> im
     public void complete(ProjectDTO projectDTO) {
         projectDTO.setStatus(Status.COMPLETE);
     }
+
+    //@Override
+//    public List<ProjectDTO> getCountedListOfProjectDto(UserDTO manager) {
+//
+//        List<ProjectDTO> projectDTOList=findAll().stream().
+//                filter(project->project.getAssignedManager().equals(manager))
+//                .map(project->{
+//
+//                    List<TaskDTO> taskListByManager=taskService.findTasksByManager(manager);
+//
+//                    int completeTaskCounts= (int) taskListByManager.stream().filter(t->t.getProjectDTO().equals(project) && t.getTaskStatus()==Status.COMPLETE).count();
+//                    int unfinishedTaskCounts=(int) taskListByManager.stream().filter(t->t.getProjectDTO().equals(project) && t.getTaskStatus()!=Status.COMPLETE).count();;
+//
+//                    project.setUnfinishedTaskCounts(unfinishedTaskCounts);
+//                    project.setCompleteTaskCounts(completeTaskCounts);
+//
+//                    return project;
+//
+//                })
+//                .collect(Collectors.toList());
+//
+//        return projectDTOList ;
+//    }
 }
--- src/main/java/com/cydeo/service/impl/TaskServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/TaskServiceImpl.java
@@ -1,6 +1,7 @@
 package com.cydeo.service.impl;
 
 import com.cydeo.dto.TaskDTO;
+import com.cydeo.dto.UserDTO;
 import com.cydeo.enums.Status;
 import com.cydeo.service.TaskService;
 import org.springframework.stereotype.Service;
@@ -8,6 +9,7 @@ import org.springframework.stereotype.Service;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Random;
+import java.util.stream.Collectors;
 
 @Service
 public class TaskServiceImpl extends AbstractMapService<TaskDTO,Long> implements TaskService {
@@ -39,12 +41,15 @@ public class TaskServiceImpl extends AbstractMapService<TaskDTO,Long> implements
 
     @Override
     public void update(TaskDTO taskDTO) {
-        if(taskDTO.getAssignedDate()==null)
-            taskDTO.setAssignedDate(LocalDate.now());
-        if(taskDTO.getTaskStatus()==null)
-            taskDTO.setTaskStatus(Status.OPEN);
+        TaskDTO foundTask=findById(taskDTO.getId());
+        taskDTO.setTaskStatus(foundTask.getTaskStatus());
+        taskDTO.setAssignedDate(foundTask.getAssignedDate());
         super.update(taskDTO.getId(),taskDTO);
     }
 
 
+    @Override
+    public List<TaskDTO> findTasksByManager(UserDTO manager) {
+        return findAll().stream().filter(task->task.getProjectDTO().getAssignedManager().equals(manager)).collect(Collectors.toList());
+    }
 }
